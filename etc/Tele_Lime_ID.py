74ce440dcdbb868ea9d9090d5c5a8383fefcec74
Matheus Bernardes <mthbernardes@gmail.com>
2016-03-07 16:40:09 -0300

    Create Tele_Lime_ID.py

---
+++ etc/Tele_Lime_ID.py
@@ -0,0 +1,27 @@
+import telepot,time
+from pprint import pprint
+
+Work = True
+
+def get_conf():
+    from ConfigParser import ConfigParser
+    config = ConfigParser()
+    config.read('Tele_lime.conf')
+    api = config.get('Telegram','api')
+    return api
+
+def message(msg):
+    global Work
+    print 'Group ID:' , msg['chat']['id']
+    print 'Group Name: ' + msg['chat']['title']
+    print
+    print 'Username ID:', msg['from']['id']
+    hprint 'Username: ' + msg['from']['username']
+    Work = False
+
+api = get_conf()
+bot = telepot.Bot(api)
+bot.notifyOnMessage(message)
+
+while Work == True:
+    time.sleep(1)
